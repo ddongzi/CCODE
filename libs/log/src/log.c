7f1235d83a353f8fd1b5979fcf8859b2f6a88a06
dongdong <jackzhangddong@163.com>
2024-04-04 14:33:04 +0800

    --

--- libs/log/src/log.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by dong on 3/29/24.
-//
-#include "log.h"
-#include "test.h"
-
--- src/message_handler /message_handler.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by dong on 3/29/24.
-//
-#include <stdio.h>
-int main()
-{
-    printf("aaa");
-    return 0;
-}
