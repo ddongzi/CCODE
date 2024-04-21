5e711918a80aff18c50688b4d47c7114dc5c9687
dongdong <jackzhangddong@163.com>
2024-04-21 23:26:03 +0800

    记录锁

---
+++ apue/chapter_14/lock_reg.c
@@ -0,0 +1,23 @@
+#include "apue.h"
+#include <fcntl.h>
+
+#define	read_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLK, F_RDLCK, (offset), (whence), (len))
+#define	readw_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLKW, F_RDLCK, (offset), (whence), (len))
+#define	write_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLK, F_WRLCK, (offset), (whence), (len))
+#define	writew_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLKW, F_WRLCK, (offset), (whence), (len))
+#define	un_lock(fd, offset, whence, len) \
+			lock_reg((fd), F_SETLK, F_UNLCK, (offset), (whence), (len))
+int lock_reg(int fd, int cmd, int type, off_t offset, int whence, off_t len)
+{
+    struct flock lock;
+    lock.l_type = type; /* F_RDLCK, F_WRLCK, F_UNLCK*/
+    lock.l_start = offset;
+    lock.l_whence = whence;
+    lock.l_len = len; /*#bytes (o means to EOF)*/
+
+    return fcntl(fd, cmd, &lock);
+}
